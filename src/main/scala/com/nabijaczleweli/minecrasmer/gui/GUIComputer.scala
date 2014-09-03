e514b886dffc78d14f51db311a192a51569a7dbe
nabijaczleweli <nabijaczleweli@gmail.com>
2014-09-03 20:00:42 +0200

    Add texture to GUIComputer

--- src/main/scala/com/nabijaczleweli/minecrasmer/gui/GUIComputer.scala
+++ src/main/scala/com/nabijaczleweli/minecrasmer/gui/GUIComputer.scala
@@ -2,12 +2,26 @@ package com.nabijaczleweli.minecrasmer.gui
 
 import cpw.mods.fml.relauncher.{Side, SideOnly}
 import net.minecraft.client.gui.GuiScreen
+import net.minecraft.util.ResourceLocation
+import org.lwjgl.opengl.GL11._
 
 @SideOnly(Side.CLIENT)
 object GUIComputer extends GuiScreen {
 	val id = 0
 
-	override def initGui() {
-		super.initGui()
+	private final val resourceGUI   = new ResourceLocation("minecrasmer", "textures/gui/computer.png")
+	private final val textureWidth  = 200
+	private final val textureHeight = 150
+	private final lazy val textureStartX = (width - textureWidth) / 2
+	private final lazy val textureStartY = (height - textureHeight) / 2
+
+	override def drawScreen(mouseX: Int, mouseY: Int, f: Float) {
+		drawDefaultBackground()
+
+		glColor4f(1.0F, 1.0F, 1.0F, 1.0F)
+		mc.renderEngine bindTexture resourceGUI
+		drawTexturedModalRect(textureStartX, textureStartY, 0, 0, textureWidth, textureHeight)
+
+		super.drawScreen(mouseX, mouseY, f)
 	}
 }
