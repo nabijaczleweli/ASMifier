7a43168db159cfb263fce246fe3c6c771b1fcd10
nabijaczleweli <nabijaczleweli@gmail.com>
2014-09-08 15:59:46 +0200

    Add translation to CreativeTab label

--- src/main/scala/com/nabijaczleweli/minecrasmer/creativetab/CreativeTabMineCrASMer.scala
+++ src/main/scala/com/nabijaczleweli/minecrasmer/creativetab/CreativeTabMineCrASMer.scala
@@ -3,8 +3,12 @@ package com.nabijaczleweli.minecrasmer.creativetab
 import com.nabijaczleweli.minecrasmer.block.BlockComputerOff
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
+import net.minecraft.util.StatCollector
 
 object CreativeTabMineCrASMer extends CreativeTabs("ASMifier") {
 	override def getTabIconItem =
 		Item getItemFromBlock BlockComputerOff
+
+	override def getTranslatedTabLabel =
+		StatCollector translateToLocal (super.getTranslatedTabLabel + ".name")
 }
