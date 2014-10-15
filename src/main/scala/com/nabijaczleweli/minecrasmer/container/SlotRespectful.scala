1c2f9fd44395147e89a3c832edb39324d61d8870
nabijaczleweli <nabijaczleweli@gmail.com>
2014-10-15 19:32:48 +0200

    Change permissions in SlotRespectful and MineCrASMerLocation. Bump forge version

--- src/main/scala/com/nabijaczleweli/minecrasmer/container/SlotRespectful.scala
+++ src/main/scala/com/nabijaczleweli/minecrasmer/container/SlotRespectful.scala
@@ -3,7 +3,7 @@ package com.nabijaczleweli.minecrasmer.container
 import net.minecraft.inventory.{IInventory, Slot}
 import net.minecraft.item.ItemStack
 
-class SlotRespectful(private val inv: IInventory, private val id: Int, private val xdisp: Int, private val ydisp: Int) extends Slot(inv, id, xdisp, ydisp) {
+class SlotRespectful(inv: IInventory, id: Int, xdisp: Int, ydisp: Int) extends Slot(inv, id, xdisp, ydisp) {
 	override def isItemValid(is: ItemStack) =
 		inventory.isItemValidForSlot(slotNumber, is)
 }
--- src/main/scala/com/nabijaczleweli/minecrasmer/resource/MineCrASMerLocation.scala
+++ src/main/scala/com/nabijaczleweli/minecrasmer/resource/MineCrASMerLocation.scala
@@ -3,4 +3,4 @@ package com.nabijaczleweli.minecrasmer.resource
 import com.nabijaczleweli.minecrasmer.reference.Reference
 import net.minecraft.util.ResourceLocation
 
-class MineCrASMerLocation(private val path: String) extends ResourceLocation(Reference.MOD_ID, path)
+class MineCrASMerLocation(path: String) extends ResourceLocation(Reference.MOD_ID, path)
