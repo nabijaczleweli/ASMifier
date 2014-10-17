6a4a28cf87f0b67c8071dd91449ccdca64b32f7b
nabijaczleweli <nabijaczleweli@gmail.com>
2014-10-17 16:25:07 +0200

    Furnaces now give player empty ItemScoop back

--- src/main/scala/com/nabijaczleweli/minecrasmer/handler/CraftingHandler.scala
+++ src/main/scala/com/nabijaczleweli/minecrasmer/handler/CraftingHandler.scala
@@ -1,15 +1,49 @@
 package com.nabijaczleweli.minecrasmer.handler
 
+import java.util.{Map => jMap}
+
 import com.nabijaczleweli.minecrasmer.item.ItemScoop
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
-import cpw.mods.fml.common.gameevent.PlayerEvent.ItemCraftedEvent
+import cpw.mods.fml.common.gameevent.PlayerEvent.{ItemCraftedEvent, ItemSmeltedEvent}
+import net.minecraft.entity.item.EntityItem
+import net.minecraft.entity.player.EntityPlayerMP
 import net.minecraft.item.ItemStack
+import net.minecraft.item.crafting.FurnaceRecipes
+
+import scala.collection.JavaConversions._
 
 object CraftingHandler {
 	@SubscribeEvent
 	def onCraftedWithScoop(event: ItemCraftedEvent) =
 		for(idx <- 0 until event.craftMatrix.getSizeInventory)
 			processStack(event.craftMatrix getStackInSlot idx)
+
+	@SubscribeEvent
+	def onScoopSmelted(event: ItemSmeltedEvent) =
+		if(!event.player.getEntityWorld.isRemote)
+			FurnaceRecipes.smelting.getSmeltingList.asInstanceOf[jMap[ItemStack, ItemStack]].entrySet filter {_.getValue.getItem == event.smelting.getItem} find {
+				_.getKey.getItem match {
+					case null =>
+						false
+					case item: ItemScoop =>
+						!item.empty
+					case _ =>
+						false
+				}
+			} match {
+				case None =>
+				case Some(entry) =>
+					val player = event.player
+					val newStack = entry.getKey.getItem getContainerItem event.smelting
+					if(player.inventory addItemStackToInventory newStack) // Stolen from BlockCauldron
+						player match {
+							case p: EntityPlayerMP =>
+								p sendContainerToPlayer p.inventoryContainer
+							case _ =>
+						}
+					else
+						player.getEntityWorld.spawnEntityInWorld(new EntityItem(player.getEntityWorld, player.posX + .5D, player.posY + 1.5D, player.posZ + 0.5D, newStack))
+			}
 
 	private def processStack(stack: ItemStack) =
 		stack match {
