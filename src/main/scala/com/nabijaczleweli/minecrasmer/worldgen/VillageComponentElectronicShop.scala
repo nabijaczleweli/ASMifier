7f2552c649ccb63702fc293943b396da9e442e48
nabijaczleweli <nabijaczleweli@gmail.com>
2014-11-25 19:19:19 +0100

    Remake the VillageComponentElectronicShop generation

--- src/main/scala/com/nabijaczleweli/minecrasmer/worldgen/VillageComponentElectronicShop.scala
+++ src/main/scala/com/nabijaczleweli/minecrasmer/worldgen/VillageComponentElectronicShop.scala
@@ -6,29 +6,51 @@ import com.nabijaczleweli.minecrasmer.entity.Villager._
 import com.nabijaczleweli.minecrasmer.reference.Reference
 import cpw.mods.fml.common.registry.VillagerRegistry.IVillageCreationHandler
 import net.minecraft.init.{Blocks, Items}
+import net.minecraft.item.{ItemStack, ItemBlock}
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.util.WeightedRandomChestContent
 import net.minecraft.world.World
 import net.minecraft.world.gen.structure.StructureVillagePieces.{PieceWeight, Start, Village}
 import net.minecraft.world.gen.structure.{StructureBoundingBox, StructureComponent, StructureVillagePieces}
 import net.minecraftforge.common.ChestGenHooks
+import net.minecraftforge.oredict.OreDictionary
+
+import scala.collection.JavaConversions._
+import scala.util.{Random => sRandom}
 
 /** HEAVILY based on `planetminecraft.com/blog/modding-trouble---adding-village-components-forge-164` */
-class VillageComponentElectronicShop(villagePiece: StructureVillagePieces.Start, par2: Int, par3Random: Random, par4StructureBoundingBox: StructureBoundingBox, par5: Int) extends Village {
-	coordBaseMode = par5
-	boundingBox = par4StructureBoundingBox
+class VillageComponentElectronicShop(villagePiece: StructureVillagePieces.Start, par2: Int, par3Random: Random, par4StructureBoundingBox: StructureBoundingBox, par5: Int) extends Village(villagePiece, par2) {
+	import VillageComponentElectronicShop.random
 
 	private var averageGroundLevel = -1
-	private var hasMadeChest = false
+	private var hasMadeChest       = false
+	private var isCarpetToggled    = random.nextBoolean()
+	private var glass              = VillageComponentElectronicShop randomBlockFromOreDict "blockGlass"
+	private var pane               = VillageComponentElectronicShop randomBlockFromOreDict "paneGlass"
+	private var fenceCarpetColor   = random nextInt 16
+
+	def this() =
+		this(null, 0, null, null, 0)
+
+	coordBaseMode = par5
+	boundingBox = par4StructureBoundingBox
 
 	protected override def func_143012_a(par1NBTTagCompound: NBTTagCompound) {
 		super.func_143012_a(par1NBTTagCompound)
 		par1NBTTagCompound.setBoolean("createdChest", hasMadeChest)
+		par1NBTTagCompound.setBoolean("notCarpet", isCarpetToggled)
+		par1NBTTagCompound.setTag("glass", glass writeToNBT new NBTTagCompound)
+		par1NBTTagCompound.setTag("pane", pane writeToNBT new NBTTagCompound)
+		par1NBTTagCompound.setInteger("carpetColor", fenceCarpetColor)
 	}
 
 	protected override def func_143011_b(par1NBTTagCompound: NBTTagCompound) {
 		super.func_143011_b(par1NBTTagCompound)
 		hasMadeChest = par1NBTTagCompound getBoolean "createdChest"
+		isCarpetToggled = par1NBTTagCompound getBoolean "notCarpet"
+		glass = ItemStack loadItemStackFromNBT (par1NBTTagCompound getCompoundTag "glass")
+		pane = ItemStack loadItemStackFromNBT (par1NBTTagCompound getCompoundTag "pane")
+		fenceCarpetColor = par1NBTTagCompound getInteger "carpetColor"
 	}
 
 	def addComponentParts(world: World, random: Random, sbb: StructureBoundingBox): Boolean = {
@@ -38,54 +60,42 @@ class VillageComponentElectronicShop(villagePiece: StructureVillagePieces.Start,
 				return true
 			boundingBox.offset(0, averageGroundLevel - boundingBox.maxY + 4, 0)
 		}
-		fillWithBlocks(world, sbb, 0, 2, 0, 6, 3, 6, Blocks.cobblestone, Blocks.cobblestone, false)
-		fillWithBlocks(world, sbb, 0, 4, 0, 6, 7, 6, Blocks.planks, Blocks.planks, false)
-		fillWithBlocks(world, sbb, 1, 2, 1, 5, 2, 5, Blocks.planks, Blocks.planks, false)
-		fillWithAir(world, sbb, 1, 3, 1, 5, 5, 5)
-		placeBlockAtCurrentPosition(world, Blocks.stone_stairs, getMetadataWithOffset(Blocks.stone_stairs, 3), 4, 2, -1, sbb)
-		placeDoorAtCurrentPosition(world, sbb, random, 4, 3, 0, getMetadataWithOffset(Blocks.wooden_door, 3))
-		for(i <- 0 until 4) {
-			placeBlockAtCurrentPosition(world, Blocks.log, 0, 0, i + 2, 0, sbb)
-			placeBlockAtCurrentPosition(world, Blocks.log, 0, 6, i + 2, 0, sbb)
-			placeBlockAtCurrentPosition(world, Blocks.log, 0, 0, i + 2, 6, sbb)
-			placeBlockAtCurrentPosition(world, Blocks.log, 0, 6, i + 2, 6, sbb)
-			if((i >= 1) && (i <= 4))
-				placeBlockAtCurrentPosition(world, Blocks.log, 0, 3, i + 2, 1, sbb)
-		}
-		for(i <- 0 until 7) {
-			placeBlockAtCurrentPosition(world, Blocks.oak_stairs, getMetadataWithOffset(Blocks.oak_stairs, 3), i, 6, -1, sbb)
-			placeBlockAtCurrentPosition(world, Blocks.oak_stairs, getMetadataWithOffset(Blocks.oak_stairs, 3), i, 7, 0, sbb)
-		}
-		for(i <- 0 until 7) {
-			placeBlockAtCurrentPosition(world, Blocks.oak_stairs, getMetadataWithOffset(Blocks.oak_stairs, 2), i, 6, 7, sbb)
-			placeBlockAtCurrentPosition(world, Blocks.oak_stairs, getMetadataWithOffset(Blocks.oak_stairs, 2), i, 7, 6, sbb)
-		}
-		fillWithBlocks(world, sbb, 1, 4, 0, 2, 5, 0, Blocks.glass_pane, Blocks.glass_pane, false)
-		placeBlockAtCurrentPosition(world, Blocks.wool, 14, 1, 3, 1, sbb)
-		placeBlockAtCurrentPosition(world, Blocks.wool, 14, 2, 3, 1, sbb)
-		placeBlockAtCurrentPosition(world, Blocks.oak_stairs, getMetadataWithOffset(Blocks.oak_stairs, 2) + 4, 3, 5, 2, sbb)
-		for(i <- 0 until 2) {
-			placeBlockAtCurrentPosition(world, Blocks.log, 0, i * 6, 4, 2, sbb)
-			placeBlockAtCurrentPosition(world, Blocks.glass_pane, 0, i * 6, 4, 3, sbb)
-			placeBlockAtCurrentPosition(world, Blocks.log, 0, i * 6, 4, 4, sbb)
-		}
-		placeBlockAtCurrentPosition(world, Blocks.planks, 1, 1, 3, 4, sbb)
-		placeBlockAtCurrentPosition(world, Blocks.planks, 1, 2, 3, 4, sbb)
-		placeBlockAtCurrentPosition(world, Blocks.planks, 1, 3, 3, 4, sbb)
-		placeBlockAtCurrentPosition(world, Blocks.planks, 1, 3, 3, 5, sbb)
-		for(i <- 0 until 4)
-			placeBlockAtCurrentPosition(world, Blocks.wooden_slab, 1, i, 5, 5, sbb)
-		placeBlockAtCurrentPosition(world, Blocks.torch, 0, 4, 5, 1, sbb)
-		if(!hasMadeChest) {
-			val i = getYWithOffset(1)
-			val j = getXWithOffset(5, 5)
-			val k = getZWithOffset(5, 5)
-			if(sbb.isVecInside(j, i, k)) {
-				hasMadeChest = true
-				generateStructureChestContents(world, sbb, random, 5, 3, 5, ChestGenHooks getInfo VillageComponentElectronicShop.ELECTRONICS_CHEST getItems random, 1 + (random nextInt 3))
-			}
-		}
-		spawnVillagers(world, sbb, 2, 3, 5, 1)
+
+		fillWithBlocks(world, sbb, 0, 2, 0, 6, 7, 6, Blocks.stonebrick, Blocks.stonebrick, false) // Main house
+		fillWithAir(world, sbb, 1, 3, 1, 5, 6, 5) // Empty space
+		placeDoorAtCurrentPosition(world, sbb, par3Random, 3, 3, 0, getMetadataWithOffset(Blocks.wooden_door, 1)) // Door
+		placeBlockAtCurrentPosition(world, Blocks.stone_brick_stairs, getMetadataWithOffset(Blocks.stone_brick_stairs, 3), 3, 2, -1, sbb) // Doorsteps
+		// Carpet
+		for(x <- 1 until 6; z <- 1 until 6)
+			if((((x + z) % 2) == 0) ^ isCarpetToggled)
+				placeBlockAtCurrentPosition(world, Blocks.carpet, 0, x, 3, z, sbb)
+			else
+				placeBlockAtCurrentPosition(world, Blocks.carpet, 8, x, 3, z, sbb)
+		// \Carpet
+		val glassBlock = glass.getItem.asInstanceOf[ItemBlock].field_150939_a
+		// Ceiling windows
+		placeBlockAtCurrentPosition(world, glassBlock, glass.getItemDamage, 2, 7, 2, sbb)
+		placeBlockAtCurrentPosition(world, glassBlock, glass.getItemDamage, 3, 7, 3, sbb)
+		placeBlockAtCurrentPosition(world, glassBlock, glass.getItemDamage, 4, 7, 4, sbb)
+		placeBlockAtCurrentPosition(world, glassBlock, glass.getItemDamage, 2, 7, 4, sbb)
+		placeBlockAtCurrentPosition(world, glassBlock, glass.getItemDamage, 4, 7, 2, sbb)
+		// \Ceiling windows
+		val paneBlock = pane.getItem.asInstanceOf[ItemBlock].field_150939_a
+		// Wall windows
+		fillWithMetadataBlocks(world, sbb, 0, 5, 2, 0, 5, 4, paneBlock, pane.getItemDamage, paneBlock, pane.getItemDamage, false)
+		fillWithMetadataBlocks(world, sbb, 6, 5, 2, 6, 5, 4, paneBlock, pane.getItemDamage, paneBlock, pane.getItemDamage, false)
+		// \Ceiling windows
+		// Fence + Toppings + Floor
+		placeBlockAtCurrentPosition(world, Blocks.nether_brick_fence, 0, 3, 3, 4, sbb)
+		placeBlockAtCurrentPosition(world, Blocks.nether_brick_fence, 0, 4, 3, 5, sbb)
+		placeBlockAtCurrentPosition(world, Blocks.nether_brick_fence, 0, 2, 3, 5, sbb)
+		placeBlockAtCurrentPosition(world, Blocks.carpet, fenceCarpetColor, 3, 4, 4, sbb)
+		placeBlockAtCurrentPosition(world, Blocks.carpet, fenceCarpetColor, 4, 4, 5, sbb)
+		placeBlockAtCurrentPosition(world, Blocks.carpet, fenceCarpetColor, 2, 4, 5, sbb)
+		placeBlockAtCurrentPosition(world, Blocks.heavy_weighted_pressure_plate, 0, 3, 3, 5, sbb)
+		// \Fence \Toppings \Floor
+
+		spawnVillagers(world, sbb, 3, 3, 5, 1)
 		true
 	}
 
@@ -99,6 +109,7 @@ object VillageComponentElectronicShop extends IVillageCreationHandler {
 	private val widX = 9
 	private val heiY = 7
 	private val lenZ = 11
+	private val random = new Random
 
 	ChestGenHooks getInfo ELECTRONICS_CHEST addItem new WeightedRandomChestContent(Items.sugar, 0, 1, 4, 85)
 	ChestGenHooks getInfo ELECTRONICS_CHEST addItem new WeightedRandomChestContent(Items.book, 0, 1, 2, 35)
@@ -120,4 +131,14 @@ object VillageComponentElectronicShop extends IVillageCreationHandler {
 
 	override def getComponentClass =
 		classOf[VillageComponentElectronicShop]
+
+	private def randomBlockFromOreDict(name: String) =
+		sRandom shuffle (OreDictionary getOres name).toSeq take 1 map { is =>
+			is.getItemDamage match {
+				case OreDictionary.WILDCARD_VALUE =>
+					is setItemDamage (random nextInt 16)
+				case _ =>
+			}
+			is
+		} apply 0
 }
